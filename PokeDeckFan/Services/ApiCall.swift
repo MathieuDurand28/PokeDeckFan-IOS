cd8ce8888462478919b5826c32f3d2292bc77075
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2024-03-10 12:18:02 +0100

    First try tabItems & begin search views + APi crédit in toolbar.

---
+++ PokeDeckFan/Services/ApiCall.swift
@@ -0,0 +1,8 @@
+//
+//  ApiCall.swift
+//  PokeDeckFan
+//
+//  Created by Mathieu Durand on 10/03/2024.
+//
+
+import Foundation
---
+++ PokeDeckFan/Views/ListTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  ListTypeView.swift
+//  PokeDeckFan
+//
+//  Created by Mathieu Durand on 10/03/2024.
+//
+
+import SwiftUI
+
+struct ListTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListTypeView()
+}
